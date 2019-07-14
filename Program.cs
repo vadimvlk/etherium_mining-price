0ae4b4daa12bd24476ab13fd0079770c728308f4
vadimvlk <vadimvlk0705@gmail.com>
2019-07-14 13:07:55 +0300

    create console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace etherium_mining_price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
